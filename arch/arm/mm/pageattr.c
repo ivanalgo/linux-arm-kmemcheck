7bf63746b10f25c4725d0ce13daf97a984067cd8
Lin Yongting <linyongting@gmail.com>
2014-06-23 00:04:36 +0800

    fix some macro and function use

--- arch/arm/mm/pageattr.c
+++ arch/arm/mm/pageattr.c
@@ -2,30 +2,33 @@
  * Copyright Huawei Corp. 2014
  * Author(s): Lin Yongting <linyongting@gmail.com>
  */
-#include <linux/mm.h>
+//#include <linux/mm.h>
+#include <asm/pgalloc.h>
+#include <asm/pgtable.h>
 
 static void split_pmd(pmd_t *pmd, unsigned long addr, unsigned long end,
-            unsigned long next, pgprot_t mask_set, pgprot_t mask_clr)
+            		pgprot_t mask_set, pgprot_t mask_clr)
 {
-    pgprot_t old_prot = pmd_val(*pmd);
-    pgprot_t new_prot = (old_prot | mask_set) & (~mask_clr);
-    unsigned long start = addr & (~PMD_MASK);
-    struct page *page = pmd_page(*pmd);
+	pgprot_t old_prot = pmd_val(*pmd);
+	pgprot_t new_prot = (old_prot | mask_set) & (~mask_clr);
+	unsigned long start = addr & (~PMD_MASK);
+	struct page *page = pmd_page(*pmd);
+	int i = 0;
 
 	/* FIXME ??? use standard code to alloc pte table */
-	pte_t *pte = early_pte_alloc(pmd, start);
+	pte_t *pte = pte_alloc_one_kernel(&init_mm, start);
 
     do {
         if (start < addr || start > end)
-            set_pte_ext(pte, mk_pte(page, old_prot));
-        else (start < end)
-            set_pte_ext(pte, mk_pte(page, new_prog));
+            set_pte_ext(pte, mk_pte(page, old_prot), 0);
+        else 
+            set_pte_ext(pte, mk_pte(page, new_prot), 0);
 
         pte++;
         page++;
         start += PAGE_SIZE;
 
-    } while (start < next);
+    } while (i++ != PTRS_PER_PTE);
 
     __pmd_populate(pmd, __pa(pte), PMD_TYPE_SECT | PMD_SECT_AP_WRITE);
 }
@@ -33,7 +36,7 @@ static void split_pmd(pmd_t *pmd, unsigned long addr, unsigned long end,
 static void alloc_init_pte(pmd_t *pmd, unsigned long addr, unsigned long end,
 				pgprot_t mask_set, pgprot_t mask_clr)
 {
-	pte_t *pte = pte_offset(pmd, addr);
+	pte_t *pte = pte_offset_kernel(pmd, addr);
         do {
 		pgprot_t old = pte_val(*pte);
 		pgprot_t new = (old | mask_set) & (~mask_clr);
@@ -56,10 +59,10 @@ static void alloc_init_pmd(pud_t *pud, unsigned long addr,
         do {
                 next = pmd_addr_end(addr, end);
 
-        	if (pmd_large(pmd)) {
+        	if (pmd_large(*pmd)) {
         		pgprot_t new_prot = (pmd_val(*pmd) | mask_set) & (~mask_clr);
             		if (new_prot != pmd_val(*pmd))
-                		split_pmd(pmd, addr, end, next, new_prot);
+                		split_pmd(pmd, addr, next, mask_set, mask_clr);
         	} else {
             		alloc_init_pte(pmd, addr, next, mask_set, mask_clr);
         	}
@@ -78,47 +81,47 @@ static void alloc_init_pud(pgd_t *pgd, unsigned long addr,
 
         do {
                 next = pud_addr_end(addr, end);
-                alloc_init_pmd(pud, addr, next, mask_set, maks_clr);
+                alloc_init_pmd(pud, addr, next, mask_set, mask_clr);
         	addr = next;
         	pud++;
         } while (next < end);
 }
 
-static void change_page_attr(unsigned long addr, int numpages,
+static void change_page_attr(unsigned long addr, int numpage,
                  pgprot_t mask_set, pgprot_t mask_clr)
 {
-    unsigned long end = addr + numpage << PAGE_SHIFT;   
-    pgd_t *pgd = pgd_offset_k(addr);
+	unsigned long end = addr + (numpage << PAGE_SHIFT);   
+	pgd_t *pgd = pgd_offset_k(addr);
+	unsigned long next;
 
-    do {
-        unsigned long next = pgd_addr_end(addr, end);
-        alloc_init_pud(pgd, addr, next, mask_set, mask_clr);
-        addr = next;
-        pgd++;
-
-    } while(next < end);
+	do {
+		next = pgd_addr_end(addr, end);
+		alloc_init_pud(pgd, addr, next, mask_set, mask_clr);
+		addr = next;
+		pgd++;
+	} while(next < end);
 }
 
 int set_memory_ro(unsigned long addr, int numpages)
 {
-    change_page_attr(addr, numpages, __prot(0), __prot(L_PTE_WRITE);
-    return 0;
+	change_page_attr(addr, numpages, __pgprot(L_PTE_RDONLY), __pgprot(0));
+	return 0;
 }
 
 int set_memory_rw(unsigned long addr, int numpages)
 {
-    change_page_attr(addr, numpages, __prot(L_PTE_WRITE), __prot(0));
-    return 0;
+	change_page_attr(addr, numpages, __pgprot(0), __pgprot(L_PTE_RDONLY));
+	return 0;
 }
 
 /* not possible */
 int set_memory_nx(unsigned long addr, int numpages)
 {
-    return 0;
+	return 0;
 }
 
 int set_memory_x(unsigned long addr, int numpages)
 {
-    return 0;
+	return 0;
 }
 
