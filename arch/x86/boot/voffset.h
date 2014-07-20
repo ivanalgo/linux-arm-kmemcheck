7f5c23e4e1cc88907c5800896f8c26350413c198
Lin Yongting <linyongting@gmail.com>
2014-07-20 11:06:51 +0800

    remove all base source code, plan to update to a new one

--- arch/x86/boot/voffset.h
+++
@@ -1,2 +0,0 @@
-#define VO__end 0xc1ba7000
-#define VO__text 0xc1000000
--- arch/x86/boot/zoffset.h
+++
@@ -1,7 +0,0 @@
-#define ZO__end 0x00507bf0
-#define ZO_input_data 0x0000006c
-#define ZO_startup_32 0x00000000
-#define ZO_z_extract_offset 0x005b5000
-#define ZO_z_extract_offset_negative 0xffa4b000
-#define ZO_z_input_len 0x004f9f4d
-#define ZO_z_output_len 0x00a9e12c
